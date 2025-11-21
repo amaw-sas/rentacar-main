44e0efefc4e6da188264490299d75b8be1ca123d
Pablo Diaz <pabloandi@gmail.com>
2025-11-21 09:50:25 -0500

    refactor picture definitions into components

---
+++ app/components/Images/Persona.vue
@@ -0,0 +1,89 @@
+<template>
+  <picture>
+    <!-- persona avif -->
+    <source
+      type="image/avif"
+      media="(min-width: 768px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fpersona.avif?alt=media&token=39468446-a9bb-4793-8c08-f334e797d36d
+      "
+      alt="video"
+      width="2000"
+      height="1620"
+      loading="lazy"
+    />
+    <!-- persona avif movil -->
+    <source
+      type="image/avif"
+      media="(max-width: 767px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fpersona-movil.avif?alt=media&token=0085aaff-80de-4260-9a36-12412ae77b4c
+      "
+      alt="video"
+      width="333"
+      height="270"
+      loading="lazy"
+    />
+    <!-- persona webp -->
+    <source
+      type="image/webp"
+      media="(min-width: 768px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fpersona.webp?alt=media&token=8d98ef71-c9b8-4357-8093-ba9cf0aaa2e8
+      "
+      alt="carros"
+      width="2000"
+      height="1620"
+      loading="lazy"
+    />
+    <!-- persona webp movil -->
+    <source
+      type="image/webp"
+      media="(max-width: 767px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fpersona-movil.avif?alt=media&token=0085aaff-80de-4260-9a36-12412ae77b4c
+      "
+      alt="carros"
+      width="333"
+      height="270"
+      loading="lazy"
+    />
+    <!-- video png -->
+    <source
+      type="image/png"
+      media="(min-width: 768px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo.png?alt=media&token=a0fa2d81-0563-4109-954e-2707ad56ae22
+      "
+      alt="video"
+      width="2000"
+      height="1620"
+      loading="lazy"
+    />
+    <!-- video movil png -->
+    <source
+      type="image/png"
+      media="(max-width: 767px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo-movil.png?alt=media&token=4af04e5d-6ae3-43c2-8d24-07acf5397562
+      "
+      alt="video"
+      width="333"
+      height="270"
+      loading="lazy"
+    />
+    <!-- video defecto -->
+    <img
+      v-youtube-lazy="videoID"
+      src="https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo.png?alt=media&token=a0fa2d81-0563-4109-954e-2707ad56ae22"
+      alt="video"
+      width="2000"
+      height="1620"
+      loading="eager"
+      class="mx-auto"
+    />
+  </picture>
+</template>
+<script lang="ts" setup>
+const videoID = "_MVNhinVYHE";
+</script>
---
+++ app/components/Images/Video.vue
@@ -0,0 +1,89 @@
+<template>
+  <picture>
+    <!-- video avif -->
+    <source
+      type="image/avif"
+      media="(min-width: 768px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo.webp?alt=media&token=424d037e-4aab-47ef-af6f-0fa0caa24e7f
+      "
+      alt="video"
+      width="2000"
+      height="1620"
+      loading="lazy"
+    />
+    <!-- video avif movil -->
+    <source
+      type="image/avif"
+      media="(max-width: 767px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo-movil.avif?alt=media&token=e5cb0fb6-d976-4218-9620-8f2873e2d053
+      "
+      alt="video"
+      width="333"
+      height="270"
+      loading="lazy"
+    />
+    <!-- video webp -->
+    <source
+      type="image/webp"
+      media="(min-width: 768px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo.webp?alt=media&token=424d037e-4aab-47ef-af6f-0fa0caa24e7f
+      "
+      alt="video"
+      width="2000"
+      height="1620"
+      loading="lazy"
+    />
+    <!-- video webp movil -->
+    <source
+      type="image/webp"
+      media="(max-width: 767px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo-movil.webp?alt=media&token=d43a8380-d542-49b3-beab-e512053d40ac
+      "
+      alt="video"
+      width="333"
+      height="270"
+      loading="lazy"
+    />
+    <!-- video png -->
+    <source
+      type="image/png"
+      media="(min-width: 768px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo.png?alt=media&token=a0fa2d81-0563-4109-954e-2707ad56ae22
+      "
+      alt="video"
+      width="2000"
+      height="1620"
+      loading="lazy"
+    />
+    <!-- video movil png -->
+    <source
+      type="image/png"
+      media="(max-width: 767px)"
+      srcset="
+        https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo-movil.png?alt=media&token=4af04e5d-6ae3-43c2-8d24-07acf5397562
+      "
+      alt="video"
+      width="333"
+      height="270"
+      loading="lazy"
+    />
+    <!-- video defecto -->
+    <img
+      v-youtube-lazy="videoID"
+      src="https://firebasestorage.googleapis.com/v0/b/rentacar-403321.firebasestorage.app/o/rentacar-main%2Falquilatucarro%2Fimg%2Fvideo.png?alt=media&token=a0fa2d81-0563-4109-954e-2707ad56ae22"
+      alt="video"
+      width="2000"
+      height="1620"
+      loading="eager"
+      class="mx-auto"
+    />
+  </picture>
+</template>
+<script lang="ts" setup>
+const videoID = "_MVNhinVYHE";
+</script>
